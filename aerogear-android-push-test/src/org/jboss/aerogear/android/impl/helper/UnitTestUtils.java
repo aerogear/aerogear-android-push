b8b4335306cf64c2b227f5d50629064fbb9e1a38
Daniel Passos <daniel@passos.me>
2014-10-17 14:25:49 -0300

    Remove unnecessary test helper methods

--- aerogear-android-push-test/src/org/jboss/aerogear/android/impl/helper/UnitTestUtils.java
+++ aerogear-android-push-test/src/org/jboss/aerogear/android/impl/helper/UnitTestUtils.java
@@ -16,16 +16,12 @@
  */
 package org.jboss.aerogear.android.impl.helper;
 
+import org.jboss.aerogear.android.impl.reflection.FieldNotFoundException;
+
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.mockito.Mockito;
-import org.jboss.aerogear.android.impl.reflection.FieldNotFoundException;
 
 public class UnitTestUtils {
 
@@ -46,57 +42,6 @@ public class UnitTestUtils {
 
     }
 
-    public static Object getPrivateField(Object target, String fieldName)
-            throws NoSuchFieldException, IllegalArgumentException,
-            IllegalAccessException {
-        Field field = target.getClass().getDeclaredField(fieldName);
-        field.setAccessible(true);
-        return field.get(target);
-    }
-
-    public static Object getSuperPrivateField(Object target, String fieldName)
-            throws NoSuchFieldException, IllegalArgumentException,
-            IllegalAccessException {
-        Field field = target.getClass().getSuperclass().getDeclaredField(fieldName);
-        field.setAccessible(true);
-        return field.get(target);
-    }
-
-    public static <T> T getPrivateField(Object target, String fieldName,
-            Class<T> type) throws NoSuchFieldException,
-            IllegalArgumentException, IllegalAccessException {
-        Field field = target.getClass().getDeclaredField(fieldName);
-        field.setAccessible(true);
-        return (T) field.get(target);
-    }
-
-    /**
-     * 
-     * This method extracts a named field, replaces it with a spy, and returns 
-     * the spy.
-     * 
-     * @param target The object requiring a hot spy injection
-     * @param fieldName the field to spy on
-     * @param type The type of the spy
-     * @return a spy which has replaced the field from fieldName
-     * 
-     * @param <T> the class of object which is being replaced with a spy.
-     * 
-     * @throws NoSuchFieldException if a field is not found. (Thrown from the java reflection API)
-     * @throws IllegalArgumentException if an argument is illegal. (Thrown from the java reflection API)
-     * @throws IllegalAccessException if access is exceptional.  (Thrown from the java reflection API)
-     */
-    public static <T> T replaceWithSpy(Object target, String fieldName,
-            Class<T> type) throws NoSuchFieldException,
-            IllegalArgumentException, IllegalAccessException {
-        Field field = target.getClass().getDeclaredField(fieldName);
-        field.setAccessible(true);
-        T object = (T) field.get(target);
-        object = Mockito.spy(object);
-        setPrivateField(target, fieldName, object);
-        return object;
-    }
-
     public static List<Field> getAllFields(List<Field> fields, Class<?> type) {
         Collections.addAll(fields, type.getDeclaredFields());
 
@@ -107,39 +52,4 @@ public class UnitTestUtils {
         return fields;
     }
 
-    public static void callMethod(Object target, String methodName) {
-        try {
-            Method method = target.getClass().getDeclaredMethod(methodName);
-            method.setAccessible(true);
-            method.invoke(target);
-        } catch (IllegalAccessException ex) {
-            Logger.getLogger(UnitTestUtils.class.getName()).log(Level.SEVERE, null, ex);
-            throw new RuntimeException(ex);
-        } catch (IllegalArgumentException ex) {
-            Logger.getLogger(UnitTestUtils.class.getName()).log(Level.SEVERE, null, ex);
-            throw new RuntimeException(ex);
-        } catch (InvocationTargetException ex) {
-            Logger.getLogger(UnitTestUtils.class.getName()).log(Level.SEVERE, null, ex);
-            throw new RuntimeException(ex);
-        } catch (NoSuchMethodException ex) {
-            Logger.getLogger(UnitTestUtils.class.getName()).log(Level.SEVERE, null, ex);
-            throw new RuntimeException(ex);
-        } catch (SecurityException ex) {
-            Logger.getLogger(UnitTestUtils.class.getName()).log(Level.SEVERE, null, ex);
-            throw new RuntimeException(ex);
-        }
-    }
-
-    public static Object getPrivateEnum(Class<?> klass, String enumName, String constantName) {
-        for (Class declaredClass : klass.getDeclaredClasses()) {
-            if (declaredClass.getCanonicalName().equals(enumName)) {
-                for (Object t : declaredClass.getEnumConstants()) {
-                    if (t.toString().equals(constantName)) {
-                        return t;
-                    }
-                }
-            }
-        }
-        return null;
-    }
 }
