f80bdd9c21180d425e56d63215c19b0ab92014f1
Daniel Passos <daniel@passos.me>
2015-05-14 15:05:32 -0300

    Add constants of keys used by UnifiedPush Server

---
+++ aerogear-android-push/src/main/java/org/jboss/aerogear/android/unifiedpush/gcm/UnifiedPushMessage.java
@@ -0,0 +1,15 @@
+package org.jboss.aerogear.android.unifiedpush.gcm;
+
+public interface UnifiedPushMessage {
+
+    /**
+     * Constant for the "push message" key
+     */
+    String MESSAGE = "alert";
+
+    /**
+     * Constant for the "push payload id" key
+     */
+    String PUSH_MESSAGE_ID = "aerogear-push-id";
+
+}
