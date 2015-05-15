7dc7f33d5377b54c880f3619d9ab2583d046126d
Daniel Passos <daniel@passos.me>
2015-05-15 16:55:03 -0300

    Format license header

--- aerogear-android-push-test/src/main/java/org/jboss/aerogear/android/unifiedpush/test/gcm/AeroGearGCMPushRegistrarTest.java
+++ aerogear-android-push-test/src/main/java/org/jboss/aerogear/android/unifiedpush/test/gcm/AeroGearGCMPushRegistrarTest.java
@@ -1,18 +1,18 @@
 /**
- * JBoss, Home of Professional Open Source Copyright Red Hat, Inc., and
- * individual contributors.
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * 	http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.jboss.aerogear.android.unifiedpush.test.gcm;
 
@@ -267,7 +267,7 @@ public class AeroGearGCMPushRegistrarTest extends PatchedActivityInstrumentation
         protected final HttpProvider mock = Mockito.mock(HttpProvider.class);
 
         public StubHttpProvider() {
-            byte[] bytes = {1};
+            byte[] bytes = { 1 };
             Mockito.doReturn(new HeaderAndBody(bytes, new HashMap<String, Object>()))
                     .when(mock)
                     .post((String) Mockito.any());
@@ -288,7 +288,7 @@ public class AeroGearGCMPushRegistrarTest extends PatchedActivityInstrumentation
         protected final HttpProvider mock = Mockito.mock(HttpProvider.class);
 
         public BrokenStubHttpProvider() {
-            byte[] bytes = {1};
+            byte[] bytes = { 1 };
             Mockito.doThrow(new HttpException(bytes, 401))
                     .when(mock)
                     .post((String) Mockito.any());
--- aerogear-android-push/src/main/java/org/jboss/aerogear/android/unifiedpush/gcm/AeroGearGCMPushRegistrar.java
+++ aerogear-android-push/src/main/java/org/jboss/aerogear/android/unifiedpush/gcm/AeroGearGCMPushRegistrar.java
@@ -1,18 +1,18 @@
 /**
- * JBoss, Home of Professional Open Source Copyright Red Hat, Inc., and
- * individual contributors.
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * 	http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.jboss.aerogear.android.unifiedpush.gcm;
 
@@ -204,9 +204,9 @@ public class AeroGearGCMPushRegistrar implements PushRegistrar, MetricsSender<Un
 
     /**
      * Unregister device from Unified Push Server.
-     *
+     * 
      * if the device isn't registered onFailure will be called
-     *
+     * 
      * @param context Android application context
      * @param callback a callback.
      */
@@ -289,12 +289,12 @@ public class AeroGearGCMPushRegistrar implements PushRegistrar, MetricsSender<Un
 
     /**
      * Send a confirmation the message was opened
-     *
+     * 
      * @param metricsMessage The id of the message received
      * @param callback a callback.
      */
     public void sendMetrics(final UnifiedPushMetricsMessage metricsMessage,
-                            final Callback<UnifiedPushMetricsMessage> callback) {
+            final Callback<UnifiedPushMetricsMessage> callback) {
         new AsyncTask<Void, Void, Exception>() {
             @Override
             protected Exception doInBackground(Void... params) {
@@ -333,7 +333,6 @@ public class AeroGearGCMPushRegistrar implements PushRegistrar, MetricsSender<Un
 
         }.execute((Void) null);
     }
-
 
     /**
      * @return Application's {@code SharedPreferences}.
