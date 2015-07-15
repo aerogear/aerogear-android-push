334095d2072f54ad37487b75103f00cf85fe529c
Summers Pittman <secondsun@gmail.com>
2015-07-15 17:36:25 -0400

    Saving registrar data to shared preferences.

---
+++ aerogear-android-push/src/main/java/org/jboss/aerogear/android/unifiedpush/gcm/GCMSharedPreferenceProvider.java
@@ -0,0 +1,26 @@
+package org.jboss.aerogear.android.unifiedpush.gcm;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import org.jboss.aerogear.android.core.Provider;
+
+/**
+ * Provides a standard shared preferences reference
+ */
+public class GCMSharedPreferenceProvider implements Provider<SharedPreferences>{
+
+    private static final String preferencesKey = GCMSharedPreferenceProvider.class.getName();
+    
+    @Override
+    public SharedPreferences get(Object... in) {
+         if (in == null || in.length != 0 || !(in[0] instanceof Context)) {
+             throw new IllegalArgumentException("get requires a single Context reference");
+         }
+         
+         Context context = (Context) in[0];
+         return PreferenceManager.getDefaultSharedPreferences(context);
+         
+    }
+    
+}
