e24dbdb94cc6c8fbac8f4b2064ebc0c141b6fb09
Erik Jan de Wit <erikjan.dewit@gmail.com>
2015-08-11 15:59:27 +0200

    added test

--- aerogear-android-push-test/src/main/java/org/jboss/aerogear/android/unifiedpush/test/gcm/UnifiedPushConfigTest.java
+++ aerogear-android-push-test/src/main/java/org/jboss/aerogear/android/unifiedpush/test/gcm/UnifiedPushConfigTest.java
@@ -1,5 +1,6 @@
 package org.jboss.aerogear.android.unifiedpush.test.gcm;
 
+import org.jboss.aerogear.android.unifiedpush.gcm.AeroGearGCMPushConfiguration;
 import org.jboss.aerogear.android.unifiedpush.gcm.UnifiedPushConfig;
 import org.junit.Assert;
 import org.junit.Test;
@@ -19,7 +20,21 @@ public class UnifiedPushConfigTest {
         config.setCategories(categories);
 
         //then
-
         Assert.assertEquals(categories, config.getCategories());
     }
+
+    @Test
+    public void shouldPushConfigSetCategories() throws Exception {
+        //given
+        AeroGearGCMPushConfiguration config = new AeroGearGCMPushConfiguration();
+        List<String> categories = Arrays.asList("cat1", "cat2");
+
+        //when
+        config.setCategories(categories);
+        config.setCategories(categories);
+
+        //then
+        Assert.assertEquals(categories, config.getCategories());
+    }
+
 }
